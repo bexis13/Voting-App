c31d73b6d61e5cfa340b0cbecb4bdd7c9908f8e0
Abose Ukhun <bexis17@gmail.com>
2017-09-17 13:14:59 +0000

    rename file from server.js to app.js, add navbar to header partial to be uniform across all pages and add container-fluid to header and footer

---
+++ app.js
@@ -0,0 +1,21 @@
+const express    = require("express"),
+      router     = require("./routes/routes"),
+      models     = require("./models/models"),
+      app        = express(),
+      path       = require("path"),
+      cors       = require("cors"),
+      bodyParser = require("body-parser"),
+      mongoose   = require("mongoose"),
+      passport   = require("passport");
+      
+//app config
+app.use(cors());
+app.use(bodyParser.json());
+const port = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(router);
+
+
+app.listen(port, ()=>{
+    console.log("app is listening on port");
+})
