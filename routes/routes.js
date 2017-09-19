a2230609bc2dd69c380839c9312e5bb82808f56d
Abose Ukhun <bexis17@gmail.com>
2017-09-19 11:08:52 +0000

    add register route and link index page to register page

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,25 @@
 const express = require("express"),
       router  = express.Router();
-      
+
+//index route   
 router.get('/', (req, res)=>{
     const _         = require("underscore");
 	
 	res.render("index", { 
+		// Underscore.js lib
+		_     : _, 
+		
+		// Template data
+		title : "Voting App"
+	});
+
+})
+
+//register route
+router.get('/register', (req, res)=>{
+    const _         = require("underscore");
+	
+	res.render("register", { 
 		// Underscore.js lib
 		_     : _, 
 		
