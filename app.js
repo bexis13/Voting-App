5b24efbf0d65f1b46fbc8d75a34461eb5088416e
Abose Ukhun <bexis17@gmail.com>
2017-09-22 16:43:01 +0000

    add app config for for user register login/logout, install passport and related modules, and add a temporary favicon

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
-const express    = require("express"),
-      router     = require("./routes/routes"),
-      models     = require("./models/models"),
-      app        = express(),
-      path       = require("path"),
-      cors       = require("cors"),
-      bodyParser = require("body-parser"),
-      mongoose   = require("mongoose"),
-      passport   = require("passport");
-      
-//app config
-app.use(cors());
+var http                  = require('http')
+, express                 = require('express')
+, app                     = express()
+, router                  = require('./routes/routes')
+, path                    = require('path')
+, favicon                 = require('serve-favicon')
+, mongoose                = require("mongoose")
+, methodOverride          = require('method-override')
+, bodyParser              = require('body-parser')
+, passport                = require('passport')
+, LocalStrategy           = require("passport-local")          
+, passportLocalMongoose   = require('passport-local-mongoose')
+, User                    = require('./models/user')
+, errorHandler            = require('errorhandler');
+
+
+
+// All environments
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : true}));
-const port = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require("express-session")({
+    secret : "secret session info",
+    resave : false,
+    saveUninitialize : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(errorHandler());
 app.use(router);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
 
-app.listen(port, ()=>{
-    console.log("app is listening on port");
+//passport config
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//redirect for all unmatched urls
+app.use(redirectUnmatched);
+function redirectUnmatched(req, res) {
+  res.send('we have no such urls.')
+}
+
+
+
+// Run server
+app.listen(process.env.PORT || 8080 || 80 || 3000, ()=>{
+  console.log("app is running on port");
 })
